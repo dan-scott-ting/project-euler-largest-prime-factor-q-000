7c9afb9fe5e39492574d1c18fad220abd7111c55
dan-scott-ting <daniel.scott.ting@gmail.com>
2016-03-10 14:58:54 +0000

    nitrous-backup at Thu Mar 10 14:58:54 UTC 2016

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num, factor = 2)
+  while factor <= num do
+    if factor == num
+      return factor
+    elsif num % factor == 0
+      num = num / factor
+      largest_prime_factor(num, factor)
+    else
+      factor += 1
+    end
+  end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,18 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(number)
+    @factor = 2
+  end
+
+  def number(number)
+    return @factor if @factor == number
+    while @factor < number do
+      if number % @factor == 0
+        number = number / @factor
+        pnumber(number)
+      else
+        @factor += 1
+      end
+    end
+  end
+end
